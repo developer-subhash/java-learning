423231516e783eac35c943c00ded62860a7cd779
subhash sam <subhash10105@gmail.com>
2024-03-17 17:19:37 +0530

    java lambda

--- lambda.java
+++ lambda.java
@@ -2,6 +2,10 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Consumer;
 
+interface LambdaFunction{
+    void run(int n);
+}
+
 public class lambda {
     public static void main(String[] args){
         System.out.println("hello world");
@@ -10,10 +14,13 @@ public class lambda {
         numbers.add(2);
         numbers.add(3);
         numbers.add(4);
-        numbers.forEach(n -> System.out.println(n));
+//        numbers.forEach(n -> System.out.println(n));
 
-        Consumer<Integer> lambdaMethod = n -> System.out.println(n);
+//        Consumer<Integer> lambdaMethod = n -> System.out.println(n);
 
-        numbers.forEach(lambdaMethod);
+        LambdaFunction var = n -> System.out.println(n);
+
+//        numbers.forEach(lambdaMethod);
+        numbers.forEach( n-> var.run(n));
     }
 }
---
+++ lambda.md
@@ -0,0 +1,7 @@
+# Java Lambda
+- https://www.w3schools.com/java/java_lambda.asp
+- lambda function is a function expression
+- (param1, param2) -> {/* code */}
+- we can store this lambda expression in a variable
+- variable type should be an interface having only one method and method signature should be same as lambda function i.e. return type and parameters all should match
+- 
